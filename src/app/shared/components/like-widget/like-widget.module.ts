c2d33b6db21c8a3f098af027e99bc875ccb6deea
Daniel Euclides <daniel.123.correa@gmail.com>
2022-09-15 17:22:40 -0300

    project started and some written tests

---
+++ src/app/shared/components/like-widget/like-widget.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule} from '@angular/common'
+import { NgModule} from '@angular/core'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+
+@NgModule({
+  declarations:[],
+  imports: [
+    CommonModule,
+    FontAwesomeModule
+  ],
+  exports:[]
+})
+export class LikeWidgetModule {}
